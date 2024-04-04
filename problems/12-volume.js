88989b404a28da6ec0ddf8e6221977d80cc6f697
danehoov <DaneHoov22@gmail.com>
2024-04-04 12:39:54 -0600

    12 solved

--- problems/12-volume.js
+++ problems/12-volume.js
@@ -13,8 +13,48 @@ function returned by recVolume should continue to return the original volume.
 
 ***********************************************************************/
 
-// Your code here 
+// function recVolume(height) {
+//   let volume = height;
+//   let measurementCount = 0
+//   return function getLength(length) {
+//     volume *= length;
 
+//     return function getWidth(width) {
+//       volume *= width
+
+//       return volume;
+//     }
+//   }
+// }
+
+// Write a function named `recVolume(height)`. The recVolume function will be passed
+// a height int and will return a function. The function returned by recVolume can
+// then be invoked two more times with a single argument number each time (one for
+// length and one for width). Once all three numbers (height, width, length) have
+// been collected return the volume of the rectangle. Any subsequent calls to the
+// function returned by recVolume should continue to return the original volume.
+
+function recVolume(height) {
+  let dimensions = [height];
+  const measurement = (num) => {
+    if (dimensions.length < 3) {
+      dimensions.push(num);
+    }
+    if (dimensions.length === 3) {
+      let sum = dimensions.reduce((acc, el) => (acc *= el));
+      return sum;
+    } else {
+      return measurement;
+    }
+  };
+  return measurement;
+}
+
+const recHeight = recVolume(15);
+const recLength = recHeight(12);
+const recWidth = recLength(12);
+
+console.log(recWidth);
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
