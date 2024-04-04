597b5f68f03411a620f7e78baff7b240cc710820
danehoov <DaneHoov22@gmail.com>
2024-04-03 22:56:51 -0600

    03 solved

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -10,9 +10,26 @@ arrowRestSum(3,5,6); // => 14
 arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
+const arrowRestSum = (...nums) => {
+  let sum = 0;
 
-// Your code here 
+  for (let num of nums) {
+    sum += num;
+  }
 
+  return sum
+
+  // for (let i = 0; i < nums.length - 1; i++) {
+  //   let el = nums[i]
+  //   num = el + nums[i + 1]
+  //   console.log(num)
+  // }
+  // return num;
+}
+
+arrowRestSum(3, 5, 6); // => 14
+arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
+arrowRestSum(0); // => 0
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
