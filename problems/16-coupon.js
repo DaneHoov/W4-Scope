ee9dae1eef0ed91c6d6f7ec022b0e21eb6c27f66
danehoov <DaneHoov22@gmail.com>
2024-04-04 22:22:19 -0600

    16 solved

--- problems/16-coupon.js
+++ problems/16-coupon.js
@@ -14,9 +14,25 @@ console.log(twentyPercent([10, 20, 30])); // [ 8, 16, 24 ]
 
 
 ***********************************************************************/
+// Write a function named: coupon(discount). The coupon function will intake a
+// number to apply as a discount, and will return a function that accepts an array
+// of prices. Every call to the function returned by coupon will return the array
+// of prices with the discount applied.
 
-// Your code here 
+const coupon = (discount) => {
 
+  return (prices) => {
+    return prices.map((price) => (price -= price * discount))
+  }
+};
+
+// Example 1:
+let tenPercent = coupon(0.1);
+console.log(tenPercent([10, 20, 30])); // [ 9, 18, 27 ]
+
+// Example 2:
+let twentyPercent = coupon(0.2);
+console.log(twentyPercent([10, 20, 30])); // [ 8, 16, 24 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
