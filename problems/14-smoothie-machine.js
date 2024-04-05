ccb5c35745e3d94a3e0c8abeab19e8d3113955a0
danehoov <DaneHoov22@gmail.com>
2024-04-05 15:36:08 -0600

    14 updated

--- problems/14-smoothie-machine.js
+++ problems/14-smoothie-machine.js
@@ -23,13 +23,25 @@ console.log(smoothie2("pineapple"));
 ***********************************************************************/
 
 const smoothieMachine = (...str) => {
-  let string = "I'm having a smoothie with"
+  let string = "I'm having a smoothie with "
   return function (...str2) {
     str = str.concat(str2)
     return string + str.join(' and ');
-   }
   }
+}
 
+let smoothie1 = smoothieMachine();
+
+console.log(smoothie1("milk"));
+// prints "I'm having a smoothie with milk"
+console.log(smoothie1("kale", "spinach"));
+// prints "I'm having a smoothie with milk and kale and spinach"
+console.log(smoothie1("honey", "pears", "berries"));
+// prints "I'm having a smoothie with milk and kale and spinach and honey and pears and berries"
+
+let smoothie2 = smoothieMachine("apples", "bananas", "berries");
+console.log(smoothie2("pineapple"));
+// prints "I'm having a smoothie with apples and bananas and berries and pineapple"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = smoothieMachine;
